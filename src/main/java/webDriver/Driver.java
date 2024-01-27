b2a4b2af51077cfa79ebe7e54443360915fbe920
Adrian <xxgulison@gmail.com>
2024-01-27 21:12:41 +0100

    setable chromedriver path

--- src/main/java/webDriver/Driver.java
+++ src/main/java/webDriver/Driver.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
+import static utils.Configurator.config;
+
 public final class Driver {
     @Getter
     private static ChromeDriver webDriver = null;
 
     public static void openDriverUrl(String url) {
+        System.setProperty("webdriver.chrome.driver", config.get("CHROMEDRIVER_PATH"));
         webDriver = new ChromeDriver(new ChromeOptions().addArguments("--headless", "--window-size=1920,1080", "--remote-allow-origins=*", "--disable-gpu", "--silent"));
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         webDriver.manage().timeouts().pageLoadTimeout(Duration.ofMinutes(1));
