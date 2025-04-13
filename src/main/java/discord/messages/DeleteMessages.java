d96f82902a7adf1aeea74248f218c098955a97d8
Guly <xxgulison@gmail.com>
2025-04-13 21:07:36 +0200

    fix delete msgs

--- src/main/java/discord/messages/DeleteMessages.java
+++ src/main/java/discord/messages/DeleteMessages.java
@@ -5,6 +5,9 @@ import discord4j.core.object.entity.channel.GuildMessageChannel;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static discord.messages.GetMessages.getChannelMessages;
@@ -14,10 +17,10 @@ public final class DeleteMessages {
 
     public static void deleteMessages(GuildMessageChannel channel) {
         try {
-            Flux<Message> messages = getChannelMessages(channel);
-            long size = messages.count().block();
-            if(size == 1) messages.blockFirst().delete().subscribe();
-            else channel.bulkDeleteMessages(messages).subscribe();
+            List<Message> messages = getChannelMessages(channel).collectList().block();
+            int size = messages.size();
+            if(size == 1) messages.get(0).delete().subscribe();
+            else channel.bulkDeleteMessages(Flux.fromIterable(messages)).subscribe();
             log.info("Deleted {} message(s)", size);
         } catch (Exception ignore) {
             log.info("Could not delete messages");
--- src/main/java/discord/messages/GetMessages.java
+++ src/main/java/discord/messages/GetMessages.java
@@ -6,6 +6,7 @@ import discord4j.core.object.entity.channel.GuildMessageChannel;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.time.Instant;
 
 import static discord.Connector.getId;
@@ -20,11 +21,16 @@ public final class GetMessages {
     public static Flux<Message> getChannelMessages(GuildMessageChannel channel, Instant from) {
         try {
             Snowflake now = Snowflake.of(from);
+
             return channel.getMessagesBefore(now)
                     .take(100)
                     .filter(message -> message.getAuthor()
                             .map(user -> user.getId().equals(Snowflake.of(getId())))
-                            .orElse(false));
+                            .orElse(false))
+                    .filter(message -> {
+                        Instant time = Instant.now().minus(Duration.ofDays(13));
+                        return message.getTimestamp().isAfter(time);
+                    });
         } catch (Exception ignore) {
             log.info("Could not get messages from channel");
             return Flux.empty();
