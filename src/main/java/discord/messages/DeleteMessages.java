fa8368733ffd500546635ccf84199116d4c74e19
Guly <xxgulison@gmail.com>
2025-04-13 20:08:14 +0200

    fix delete messages if only 1

--- src/main/java/discord/messages/DeleteMessages.java
+++ src/main/java/discord/messages/DeleteMessages.java
@@ -5,6 +5,8 @@ import discord4j.core.object.entity.channel.GuildMessageChannel;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static discord.messages.GetMessages.getChannelMessages;
 
 @Slf4j
@@ -13,9 +15,10 @@ public final class DeleteMessages {
     public static void deleteMessages(GuildMessageChannel channel) {
         try {
             Flux<Message> messages = getChannelMessages(channel);
-            channel.bulkDeleteMessages(messages).subscribe();
-            channel.bulkDeleteMessages(messages).subscribe();
-            log.info("Deleted " + messages.count().block() + " messages");
+            long size = messages.count().block();
+            if(size == 1) messages.blockFirst().delete().subscribe();
+            else channel.bulkDeleteMessages(messages).subscribe();
+            log.info("Deleted {} message(s)", size);
         } catch (Exception ignore) {
             log.info("Could not delete messages");
         }
